31f8d892c43bf02284ac78486296256df186a0d1
mopdc <mariana.o.paixao@gmail.com>
2019-07-02 20:29:52 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+
+
+var divs = ["oi", "sobre", "apple-menu", "descomplica"];
+    var visibleDivId = null;
+    function toggleVisibility(divId) {
+      if(visibleDivId === divId) {
+        visibleDivId = null;
+      } else {
+        visibleDivId = divId;
+      }
+      hideNonVisibleDivs();
+    }
+    function hideNonVisibleDivs() {
+      var i, divId, div;
+      for(i = 0; i < divs.length; i++) {
+        divId = divs[i];
+        div = document.getElementById(divId);
+        if(visibleDivId === divId) {
+          div.style.display = "block";
+        } else {
+          div.style.display = "none";
+        }
+      }
+    }
